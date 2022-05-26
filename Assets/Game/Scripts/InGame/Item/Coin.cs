1824fcc2e9fe2262f720c0de1ce460278a8e2b17
lehungphuong99 <lehungphuong99@gmail.com>
2022-05-26 17:26:56 +0700

    sound

--- Assets/Game/Scripts/InGame/Item/Coin.cs
+++ Assets/Game/Scripts/InGame/Item/Coin.cs
@@ -1,13 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Coin : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision) {
         Player player = collision.gameObject.GetComponent<Player>();
         if(player!= null) {
-            if(DataManager.Instance != null) {
+            if(DataManager.Instance != null) 
+            {
+                SoundManager.Instance.PlaySoundGoldCollect();
                 int random = Random.Range(1,5);
                 //DataManager.Instance.PlayerData.AddItem(new ItemStack(ItemID.COIN,1));
                 InGameManager.Instance.CoinInGame += random;
