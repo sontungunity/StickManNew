eaeebe00f20af89356cadaf476f9615a2ef7e5cf
lehungphuong99 <lehungphuong99@gmail.com>
2022-06-08 09:08:31 +0700

    maps

--- Assets/Game/Scripts/InGame/Item/ChestCS.cs
+++ Assets/Game/Scripts/InGame/Item/ChestCS.cs
@@ -1,6 +1,7 @@
 using Spine.Unity;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class ChestCS : MonoBehaviour
@@ -8,6 +9,9 @@ public class ChestCS : MonoBehaviour
     [SerializeField] private SpineBase spine;
     [SerializeField,SpineAnimation] private string animIdle,animOpen,animOpenIdle;
     [SerializeField] private Transform point;
+    [SerializeField] private ParticleSystem par;
+    [SerializeField] private GameObject blink, loot;
+
     public bool open;
     private void Start() {
         spine.SetAnim(0, animIdle, true);
@@ -20,11 +24,24 @@ public class ChestCS : MonoBehaviour
         }
         Player player = collision.transform.parent.GetComponent<Player>();
         if(player != null) {
+            // par.Play();
+            StartCoroutine(playPar());
+
             open = true;
             spine.SetAnim(0,animOpen,false,()=> {
-                SpawnerCoin.Instance.SpawnerII(point.position,10);
+                SpawnerCoin.Instance.SpawnerII(point.position,15);
                 spine.SetAnim(0, animOpenIdle, true);
             });
         }
+    }
+
+    IEnumerator playPar()
+    {
+        var playPar = par.Spawn();
+        playPar.transform.position = transform.position;
+        playPar.Play();
+        yield return new WaitForSeconds(1.5f);
+        blink.SetActive(false);
+        loot.SetActive(false);
     }
 }
--- Assets/Game/Scripts/InGame/Item/SwordCS.cs
+++ Assets/Game/Scripts/InGame/Item/SwordCS.cs
@@ -1,13 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SwordCS : MonoBehaviour
 {
     [SerializeField] private WeaponID weaponID;
+    [SerializeField] private SpriteRenderer img;
+    [SerializeField] private ParticleSystem par;
+
+    private void Start()
+    {
+        WeaponData data = weaponID.GetDataWeaponByID();
+        img.sprite = data.Icon;
+        this.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+        // transform.rotation = new Quaternion(0, 0, -40, 0);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
         Player player = collision.transform.parent.GetComponent<Player>();
-        if(player != null) {
+        if(player != null) 
+        {
+            var spawnPar = par.Spawn();
+            spawnPar.transform.position = this.transform.position;
+            spawnPar.Play();
+
             AdsManager.Instance.ShowRewarded((value)=> {
                 if(value) {
                     player.SetWeapon(weaponID.GetDataWeaponByID());
